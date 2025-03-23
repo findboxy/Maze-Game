f8674ea35b3291edb7389e712c4a3203bd058571
Zac Kenny <zackennyart@gmail.com>
2025-03-23 10:33:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -557,8 +557,8 @@ function rand(max) {
       ctx.canvas.width = viewHeight - viewHeight / 100;
       ctx.canvas.height = viewHeight - viewHeight / 100;
     } else {
-      ctx.canvas.width = viewWidth - viewWidth / 100;
-      ctx.canvas.height = viewWidth - viewWidth / 100;
+      ctx.canvas.width = viewWidth - viewWidth / 120;
+      ctx.canvas.height = viewWidth - viewWidth / 120;
     }
     cellSize = mazeCanvas.width / difficulty;
     if (player != null) {
